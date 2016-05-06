4a323439739537ac902d993e6e4e2f535597d7b2
Zoe Chen <shzoechen@gmail.com>
2016-05-06 08:40:21 -0700

    [progress] editProfile add location works

--- client/profile/editProfile.controller.js
+++ client/profile/editProfile.controller.js
@@ -1,5 +1,14 @@
 angular.module('finder.editProfile', [])
 
-.controller('EditProfileController', function($scope, $window, $state, HttpRequest) {
-	$scope.cuisineTypes = ["Chinese", "American"]
+.controller('EditProfileController', function($scope, $window, $state) {
+	$scope.cuisineTypes = ['Chinese', 'American'];
+	$scope.timeList = ['12:00am', '8:00am', '9:00am', '10:00am'];
+	$scope.addLocation = false;
+	$scope.locations = [];
+
+	$scope.editLocation = function(){
+		console.log('location, housr', $scope.address, $scope.start1)
+		var location = {};
+		location.address = $scope.address;
+	}
 });
