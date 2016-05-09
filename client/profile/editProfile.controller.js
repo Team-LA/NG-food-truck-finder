7da7d2769a851aeadb466adb1fcca3869bf34989
Zoe Chen <shzoechen@gmail.com>
2016-05-09 10:23:24 -0700

    [Progress] edit profile

--- client/profile/editProfile.controller.js
+++ client/profile/editProfile.controller.js
@@ -7,8 +7,18 @@ angular.module('finder.editProfile', [])
 	$scope.locations = [];
 
 	$scope.editLocation = function(){
-		console.log('location, housr', $scope.address, $scope.start1)
+		$scope.addLocation = false;
 		var location = {};
 		location.address = $scope.address;
+		location.hours = {};
+		location.hours["1"] = [$scope.start1, $scope.end1];
+		location.hours["2"] = [$scope.start2, $scope.end2];
+		location.hours["3"] = [$scope.start3, $scope.end3];
+		location.hours["4"] = [$scope.start4, $scope.end4];
+		location.hours["5"] = [$scope.start5, $scope.end5];
+		location.hours["6"] = [$scope.start6, $scope.end6];
+		location.hours["0"] = [$scope.start0, $scope.end0];
+		$scope.locations.push(location);
+		console.log('location', $scope.locations)
 	}
 });
